29f2e431c4d5807a21a8fef9c0661a382c84a090
Anaf02 <100018619+Anaf02@users.noreply.github.com>
2025-12-03 09:41:39 +0200

    coroutines-lab-solution

--- app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
+++ app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -16,7 +16,15 @@
 package com.example.racetracker
 
 import com.example.racetracker.ui.RaceParticipant
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class RaceParticipantTest {
     private val raceParticipant = RaceParticipant(
         name = "Test",
@@ -25,4 +33,22 @@ class RaceParticipantTest {
         initialProgress = 0,
         progressIncrement = 1
     )
+
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdated() = runTest {
+        val expectedProgress = 100
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.maxProgress * raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
 }
